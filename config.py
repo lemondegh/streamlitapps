79847d98c88032bae0bf9140c4476a15e4cf0c9f
lemondegh <lemonde@misoinfo.co.kr>
2023-03-14 15:44:40 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+API_KEY ='vEcBp99%2BOW96a4dIYtYFUZIbrBlTYrmAjiKtphiTPtoUluYaGn4k1JTSao0gW5rnVehVy1SmckQ4f8Jla%2F1Wdw%3D%3D'
+SERVICE = '/getBidPblancListInfoServcPPSSrch01'
+URL = 'https://apis.data.go.kr/1230000/BidPublicInfoService04'
