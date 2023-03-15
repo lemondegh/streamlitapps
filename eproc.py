6feec8250e086107e5672459010147299a0304f7
lemondegh <lemonde@misoinfo.co.kr>
2023-03-15 18:11:11 +0900

    Add files via upload

--- eproc.py
+++ eproc.py
@@ -6,8 +6,10 @@ from urllib.parse import urlencode, unquote
 import requests
 import json
 import math
+import pandas as pd
 
 from config import *
+
 
 today = date.today()
 
@@ -26,30 +28,63 @@ month_s = int(prev_month_today.split('-')[1])
 day_s = int(prev_month_today.split('-')[2])
 # print(year, month, day)
 
+df = pd.DataFrame()
+
 ROWS = 20
 date_option = ['공고일', '개찰일']
 duration_option = ['From/To 지정', '최근 1개월', '최근 3개월']
 work_option = ['전체', '물품', '공사', '용역', '리스', '외자', '비축', '기타', '민간']
 
 org_option = ['공고기관', '수요기관']    
-        
-with st.form('기관 검색'):
-    col1, col2 = st.columns([2, 8])
-    with col1:
-        org_str = st.selectbox('기관명', org_option)
-    with col2:
-        org_input = st.text_input('기관명 검색')
 
-    col1, col2 = st.columns([9, 1])
-    with col1:
-        st.empty()
-    with col2:
-        search = st.form_submit_button("검색", use_container_width=True)
+# if 'df' not in st.session_state:
+#     st.session_state.df = pd.DataFrame()
+
+# def check_df():
+#     # global df
+#     print('-----')
+#     print(st.session_state.df)
     
+# def load_df():
+#     df = pd.read_csv('org_code.csv', sep='@', index_col=0)
+#     df = df.loc[df['전체기관명'].str.contains(org_input, case=False)]
+#     df.insert(0, '선택', False)
+#     # df = st.experimental_data_editor(st.session_state.df, num_rows='fixed', use_container_width=True, \
+#     #     on_change=check_df, args=(st.session_state.df,))
+#     df = st.experimental_data_editor(st.session_state.df, num_rows='fixed', use_container_width=True)
+#     st.button('Done', on_click=check_df)
+#     # st.info('Search..................')
+                    
+# with st.form('기관 검색'):
+# col1, col2 = st.columns([2, 8])
+# with col1:
+#     org_str = st.selectbox('기관명', org_option)
+# with col2:
+#     org_input = st.text_input('기관명 검색')
+
+# col1, col2 = st.columns([9, 1])
+# with col1:
+#     st.empty()
+# with col2:
+#     st.button("검색", on_click=load_df)
+
+# if search:
+#     df = df.loc[df['전체기관명'].str.contains(org_input, case=False)]
+#     df.insert(0, '선택', False)
+#     # df['선택'] = False
+#     # print(df.columns.tolist())
+#     print(df)
+#     # df = df[list(('선택', '기관코드', '전체기관명'))]
+#     # df['Select'] = False
+#     # first_col = df.pop['선택']
+#     selected_df = st.experimental_data_editor(df, num_rows='fixed', use_container_width=True, on_change=check_df)
+#     print(selected_df)
+#     # st.info('Search..................')
+        
 with st.form('Prompt'):
     col1, col2 = st.columns([2, 8])
     with col1:
-        work_type_str = st.selectbox('업무구분', work_option)
+        work_type_str = st.selectbox('업무구분', work_option, index=3)
     with col2:
         title = st.text_input('공고명 검색', placeholder='공고명에 포함되는 문자열') 
 
@@ -64,12 +99,31 @@ with st.form('Prompt'):
         duration_str = st.selectbox('기간', duration_option, label_visibility='hidden')  
 
     disp_rows = st.number_input('샘플 미리보기 라인 수', min_value=1, max_value=10000, value=10)
-    
+
+    # with st.expander('Columns'):
+    #     col1, col2 = st.columns([2, 8])
+    #     with col1:
+    #         st.checkbox('전체 보기')
+    #     with col2:
+    #         all_cols = df.columns.tolist()
+    #         print('All columns : ', all_cols)
+    #         def_cols = ['입찰공고번호', '입찰공고차수', '입찰공고명', '공고기관명', '수요기관명', '입찰공고일시', '입찰마감일시', '공동수급방식명', '공고종류명']
+    #         st.multiselect('Select columns', all_cols, ['입찰공고번호', '입찰공고차수', '입찰공고명', '공고기관명', '수요기관명', '입찰공고일시', '입찰마감일시', '공동수급방식명', '공고종류명'])
+
+    # df = pd.read_csv('org_code.csv', sep='@', index_col=0)
+    # df = df.loc[df['전체기관명'].str.contains(org_input, case=False)]
+    # df.insert(0, '선택', False)
+    # # df = st.experimental_data_editor(st.session_state.df, num_rows='fixed', use_container_width=True, \
+    # #     on_change=check_df, args=(st.session_state.df,))
+    # df = st.experimental_data_editor(st.session_state.df, num_rows='fixed', use_container_width=True)
+
+    # org_input = st.text_input('기관명 검색')
+    # print('IIII : ', org_input)
     submitted = st.form_submit_button("검 색", use_container_width=True)
     
 if submitted:
     error = False
-    
+
     title_len = len(title.strip())
     if title_len == 0:
         st.warning('검색어가 입력되지 않았습니다.', icon="⚠️")
@@ -165,7 +219,7 @@ if submitted:
 
         # Create a dictionary mapping the old column names to the new ones
         new_names = mapping["new"].to_dict()
-
+        print('New names : ', new_names)
         # Rename the columns using the dictionary
         df = df.rename(columns=new_names)
 
@@ -174,6 +228,8 @@ if submitted:
         print(df)
         len = len(df)
         
+        # remove columns
+        df = df[['입찰공고번호', '입찰공고차수', '입찰공고명', '공고기관명', '수요기관명', '입찰공고일시', '입찰마감일시', '공동수급방식명', '공고종류명']]
         col1, col2, col3 = st.columns(3)
         with col1:
             st.text('Total rows :' + str(len))
