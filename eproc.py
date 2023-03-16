b5cf24c6e35c19963904041d78b2b55b4b726d16
lemondegh <lemonde@misoinfo.co.kr>
2023-03-16 12:54:47 +0900

    Add files via upload

--- eproc.py
+++ eproc.py
@@ -10,7 +10,6 @@ import pandas as pd
 
 from config import *
 
-
 today = date.today()
 
 # print(today)
@@ -18,7 +17,6 @@ today_str = str(today)
 year = int(today_str.split('-')[0])
 month = int(today_str.split('-')[1])
 day = int(today_str.split('-')[2])
-# print(year, month, day)
 
 prev_month_today = today - relativedelta(months=1)
 prev_month_today = str(prev_month_today)
@@ -26,9 +24,12 @@ prev_month_today = str(prev_month_today)
 year_s = int(prev_month_today.split('-')[0])
 month_s = int(prev_month_today.split('-')[1])
 day_s = int(prev_month_today.split('-')[2])
-# print(year, month, day)
 
 df = pd.DataFrame()
+
+if 'new_date_s' not in st.session_state:
+    st.session_state.duration_str = 'From/To 지정'
+    st.session_state.new_date_s = datetime.date(year_s, month_s, day_s)
 
 ROWS = 20
 date_option = ['공고일', '개찰일']
@@ -36,97 +37,58 @@ duration_option = ['From/To 지정', '최근 1개월', '최근 3개월']
 work_option = ['전체', '물품', '공사', '용역', '리스', '외자', '비축', '기타', '민간']
 
 org_option = ['공고기관', '수요기관']    
+  
+def change_duration():
+    print('duration_option : ', duration_option)
+    if st.session_state.duration_str == duration_option[1]: # 1 month
+        print('최근 1개월', duration_str)
+        st.session_state.new_date_s = datetime.date(year_s, month_s, day_s)
+    elif st.session_state.duration_str == duration_option[2]: # 3 month
+        print('최근 3개월', duration_str)
+        three_month_today = today - relativedelta(months=3)
+        three_month_today = str(three_month_today)
 
-# if 'df' not in st.session_state:
-#     st.session_state.df = pd.DataFrame()
+        year_s3 = int(three_month_today.split('-')[0])
+        month_s3 = int(three_month_today.split('-')[1])
+        day_s3 = int(three_month_today.split('-')[2])
+        st.session_state.new_date_s = datetime.date(year_s3, month_s3, day_s3)
+            
+# with st.form('Prompt'):
+col1, col2 = st.columns([2, 8])
+with col1:
+    work_type_str = st.selectbox('업무구분', work_option, index=3)
+with col2:
+    title = st.text_input('공고명 검색', placeholder='공고명에 포함되는 문자열') 
 
-# def check_df():
-#     # global df
-#     print('-----')
-#     print(st.session_state.df)
-    
-# def load_df():
-#     df = pd.read_csv('org_code.csv', sep='@', index_col=0)
-#     df = df.loc[df['전체기관명'].str.contains(org_input, case=False)]
-#     df.insert(0, '선택', False)
-#     # df = st.experimental_data_editor(st.session_state.df, num_rows='fixed', use_container_width=True, \
-#     #     on_change=check_df, args=(st.session_state.df,))
-#     df = st.experimental_data_editor(st.session_state.df, num_rows='fixed', use_container_width=True)
-#     st.button('Done', on_click=check_df)
-#     # st.info('Search..................')
-                    
-# with st.form('기관 검색'):
-# col1, col2 = st.columns([2, 8])
-# with col1:
-#     org_str = st.selectbox('기관명', org_option)
-# with col2:
-#     org_input = st.text_input('기관명 검색')
+col1, col2, col3, col4 = st.columns([2,2,2,4])
+with col1:
+    date_type_str = st.selectbox('기준 일자 :', date_option)
+with col2:
+    date_s = str(st.date_input('From', key='new_date_s')).replace('-', '')
+with col3:
+    date_e = str(st.date_input('To', datetime.date(year, month, day))).replace('-', '')
+with col4:
+    duration_str = st.selectbox('기간', duration_option, key='duration_str', label_visibility='hidden', on_change=change_duration)  
 
-# col1, col2 = st.columns([9, 1])
-# with col1:
-#     st.empty()
-# with col2:
-#     st.button("검색", on_click=load_df)
+disp_rows = st.number_input('샘플 미리보기 라인 수', min_value=1, max_value=10000, value=10)
 
-# if search:
-#     df = df.loc[df['전체기관명'].str.contains(org_input, case=False)]
-#     df.insert(0, '선택', False)
-#     # df['선택'] = False
-#     # print(df.columns.tolist())
-#     print(df)
-#     # df = df[list(('선택', '기관코드', '전체기관명'))]
-#     # df['Select'] = False
-#     # first_col = df.pop['선택']
-#     selected_df = st.experimental_data_editor(df, num_rows='fixed', use_container_width=True, on_change=check_df)
-#     print(selected_df)
-#     # st.info('Search..................')
-        
-with st.form('Prompt'):
-    col1, col2 = st.columns([2, 8])
-    with col1:
-        work_type_str = st.selectbox('업무구분', work_option, index=3)
-    with col2:
-        title = st.text_input('공고명 검색', placeholder='공고명에 포함되는 문자열') 
+col1, col2 = st.columns([8, 2])
+with col1:
+    remove_dup = st.checkbox('최종 공고만 검색', value=True)
+with col2:
+    get_data = st.button('데이터 검색')
 
-    col1, col2, col3, col4 = st.columns([2,2,2,4])
-    with col1:
-        date_type_str = st.selectbox('기준 일자 :', date_option)
-    with col2:
-        date_s = str(st.date_input('From', datetime.date(year_s, month_s, day_s))).replace('-', '')
-    with col3:
-        date_e = str(st.date_input('To', datetime.date(year, month, day))).replace('-', '')
-    with col4:
-        duration_str = st.selectbox('기간', duration_option, label_visibility='hidden')  
-
-    disp_rows = st.number_input('샘플 미리보기 라인 수', min_value=1, max_value=10000, value=10)
-
-    # with st.expander('Columns'):
-    #     col1, col2 = st.columns([2, 8])
-    #     with col1:
-    #         st.checkbox('전체 보기')
-    #     with col2:
-    #         all_cols = df.columns.tolist()
-    #         print('All columns : ', all_cols)
-    #         def_cols = ['입찰공고번호', '입찰공고차수', '입찰공고명', '공고기관명', '수요기관명', '입찰공고일시', '입찰마감일시', '공동수급방식명', '공고종류명']
-    #         st.multiselect('Select columns', all_cols, ['입찰공고번호', '입찰공고차수', '입찰공고명', '공고기관명', '수요기관명', '입찰공고일시', '입찰마감일시', '공동수급방식명', '공고종류명'])
-
-    # df = pd.read_csv('org_code.csv', sep='@', index_col=0)
-    # df = df.loc[df['전체기관명'].str.contains(org_input, case=False)]
-    # df.insert(0, '선택', False)
-    # # df = st.experimental_data_editor(st.session_state.df, num_rows='fixed', use_container_width=True, \
-    # #     on_change=check_df, args=(st.session_state.df,))
-    # df = st.experimental_data_editor(st.session_state.df, num_rows='fixed', use_container_width=True)
-
-    # org_input = st.text_input('기관명 검색')
-    # print('IIII : ', org_input)
-    submitted = st.form_submit_button("검 색", use_container_width=True)
-    
-if submitted:
+if get_data:
     error = False
 
+    if remove_dup:
+        print('************Remove duplicated rows....')
+    else:
+        print('************Do not remove duplicated rows....')
+        
     title_len = len(title.strip())
     if title_len == 0:
-        st.warning('검색어가 입력되지 않았습니다.', icon="⚠️")
+        st.warning('공고명 검색어가 입력되지 않았습니다.', icon="⚠️")
     else:
         url = 'https://apis.data.go.kr/1230000/BidPublicInfoService04/getBidPblancListEvaluationIndstrytyMfrcInfo01'
         url = URL + SERVICE
@@ -135,7 +97,7 @@ if submitted:
             date_type = 1
         else:
             date_type = 2
-            
+                       
         queryString = "?" + urlencode(
             { 
             "ServiceKey": unquote(API_KEY), 
@@ -159,9 +121,6 @@ if submitted:
             r_dict = json.loads(response.text)
         except:
             print('Error : \n', response.text)
-            
-            
-        # json_object = json.loads(json_str)
 
         print(r_dict)
 
@@ -225,11 +184,18 @@ if submitted:
 
         # Save the renamed DataFrame
         # df.to_csv(r'.\Result\result.csv', index=False, encoding='utf-8-sig')
+        
+        if remove_dup:
+            # Remove duplicated rows by specific column
+            df = df.sort_values('입찰공고차수', ascending=False).drop_duplicates('입찰공고번호').reset_index(drop=True)
+
         print(df)
         len = len(df)
         
         # remove columns
-        df = df[['입찰공고번호', '입찰공고차수', '입찰공고명', '공고기관명', '수요기관명', '입찰공고일시', '입찰마감일시', '공동수급방식명', '공고종류명']]
+        # df = df[['입찰공고번호', '입찰공고차수', '입찰공고명', '공고기관명', '수요기관명', '입찰공고일시', '입찰마감일시', '공동수급방식명', '공고종류명']]
+        df = df.astype({'입찰공고번호':str})
+        df = df.sort_values('입찰공고일시', ascending=False)
         col1, col2, col3 = st.columns(3)
         with col1:
             st.text('Total rows :' + str(len))
@@ -245,4 +211,4 @@ if submitted:
                 mime='text/csv',
             )   
             
-        st.dataframe(df.head(disp_rows))        
+        st.dataframe(df.head(disp_rows))     
